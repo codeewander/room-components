2a2a93abd25b9267f75b20066be400939a2913ba
Kira <changru.yang02@gmail.com>
2023-11-27 11:55:15 +0800

    feat: initial environment setting

---
+++ webpack/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve("./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  stats: {
+    children: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve("./build"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./src/index.html"),
+    }),
+  ],
+};
